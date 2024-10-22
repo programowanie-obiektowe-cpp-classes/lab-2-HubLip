a9514463bc7aec81ffcbdee34406555626ddbdb6
HubLip <01171666@pw.edu.pl>
2024-10-22 15:47:42 +0200

    Zejsciowka2.0

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,24 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* resource;
+public:
+
+    ResourceManager()
+    {
+        resource = new Resource(); // ResourceManager tworzy obiekt Resource
+    }
+
+    ResourceManager(const ResourceManager&) = delete;
+    Resource& operator=(const Resource&) = delete;
+
+    double get (Resource& res)
+    {
+        return res.get();
+    }
+
+    ~ResourceManager() {
+        delete resource;
+    }
 };
