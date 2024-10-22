11cb00a8365c733d5a0f4bc3156fe39392dacb40
HubLip <01171666@pw.edu.pl>
2024-10-22 16:15:49 +0200

    Zejsciowka2.2

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -5,7 +5,7 @@
 class ResourceManager
 {
 private:
-    Resource* resource; //Wskaznik typu Resource o nazwie resource.
+    Resource *resource; //Wskaznik typu Resource o nazwie resource.
 public:
 
     ResourceManager()
@@ -13,8 +13,25 @@ public:
         resource = new Resource(); // ResourceManager tworzy obiekt Resource
     }
 
-    ResourceManager(const ResourceManager&) = delete;
-    ResourceManager& operator=(const ResourceManager&) = delete;
+    //Poprawne kopiowanie czyli przy kopiowaniu ResourceManager, nowy obiekt ma miec swoja wlasna kopie obiektu Resource
+    ResourceManager(const ResourceManager& res)
+    {
+        resource = new Resource(*res.resource); //uzywamy *res poniewaz chcemy skopiowac sam obiekt a nie adres na jaki wskaznik wskazuje
+    }
+
+    ResourceManager& operator=(const ResourceManager& res)
+    {
+        if (this == &res) //&res zwraca adres res, samo res jest referencja do obiektu typu ResourceManager zatem aby odniesc sie do adresu trzeba uzyc &res (& to operator adresu).
+        {
+            return *this;
+        }
+
+        delete resource;
+
+        resource = new Resource(*res.resource);
+
+        return *this;
+    }
 
     double get()
     {
